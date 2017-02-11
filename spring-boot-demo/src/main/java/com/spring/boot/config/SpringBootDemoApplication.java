39a7e36e5d49f6a8ba20c304fb723371284c182c
Sumit Gupta <sumitgupta28@gmail.com>
2017-02-11 13:10:04 -0600

    adding swagger.json

--- spring-boot-demo/src/main/java/com/spring/boot/config/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/spring/boot/config/SpringBootDemoApplication.java
@@ -1,9 +1,13 @@
-package com.spring.boot.demo;
+package com.spring.boot.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.spring.boot")
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
